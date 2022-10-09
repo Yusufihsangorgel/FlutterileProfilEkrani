eb21d39b35f6abf27b864a5b68c408d2e4bb3098
Yusufihsangorgel <87409084+Yusufihsangorgel@users.noreply.github.com>
2022-10-09 20:14:31 +0300

    import hatası düzeltme

--- myprofile/lib/main.dart
+++ myprofile/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:deneme/myprofile_extension.dart';
+import 'package:myprofile/myprofile_extension.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
